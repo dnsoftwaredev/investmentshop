2d605d0e72c5fcbbda6936ddacc0781f14a80a8e
Danh <dnsoftwarestore@gmail.com>
2021-06-28 15:16:19 -0500

    simple backend completed

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
 
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products');
+            // object in return is res => res.data has the data
+
+            setProducts(data);
+        };
+
+        fetchProducts();
+    }, []);
+
     return (
         <>
             <h1>Latest Products</h1>
