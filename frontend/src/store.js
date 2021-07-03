ccc94f9d0399c00455e78df23f3dd32e2608ef58
Danh <dnsoftwarestore@gmail.com>
2021-07-03 11:52:52 -0500

    shopping cart is done, removal add etc... front end is done manipulation of backend will be done much later

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,20 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
-
+import { cartReducer } from './reducers/cartReducers';
 // combineReducers is to be used to combine all reducers into a single thing that can be passed to the store
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 
-const initialState = {};
+// grab the cart items from local storage
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage }
+};
 
 const middleware = [thunk];
 
