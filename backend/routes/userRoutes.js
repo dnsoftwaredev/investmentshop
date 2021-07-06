6cd11d734649f30c51a5383b1d1ed60598eccfef
Danh <dnsoftwarestore@gmail.com>
2021-07-06 13:40:30 -0500

    backend registration, authorization + authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
+// this is how you implement middleware just put it as first argument
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
